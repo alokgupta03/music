cc69bdf30c3eea5b90bb44b41e9f5a2560020870
alokgupta03 <chmcs2026.alok.gupta@gmail.com>
2026-03-07 14:46:59 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || 'https://backend-m88i.onrender.com',
+    baseURL: import.meta.env.VITE_BACKEND_URL || 'https://backend-m88i.onrender.com/songs',
     headers: {
         'Content-Type': 'application/json'
     }
